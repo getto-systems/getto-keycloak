a92ab776c41771c9392653cf1683f7e49d7a6de3
shun <shun@getto.systems>
2018-12-10 15:23:00 +0000

    add: gitlab-ci

---
+++ bin/push_tags.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git remote add super https://gett-systems:$GITLAB_ACCESS_TOKEN@gitlab.com/getto-systems-labo/keycloak.git
+git remote add github https://getto-systems:$GITHUB_ACCESS_TOKEN@github.com/getto-systems/keycloak.git
+git tag $(cat .release-version)
+git push super HEAD:master --tags
+git push github HEAD:master --tags
